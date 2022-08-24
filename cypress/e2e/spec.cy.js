8f9591f959ebf7d29822c99e7f57ca99db9c2654
Rachel <Raquel.rudermano@gmail.com>
2022-08-24 13:42:59 -0700

    add teset

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('cypress/fixtures/scroll-smooth.html')
+    cy.get('html').should('have.css', 'scroll-behavior', 'smooth')
+    cy.contains('Click Me to Smooth Scroll to Section 2 Below').click()
+    cy.wait(1000) // wait to demonstrate smooth scroll in UI
+    cy.contains('Click Me to Smooth Scroll to Section 1 Above').click()
   })
 })
